fdc47c6dc99ac51a34bc1df298bc9a00d9409da0
Arona Jones <warlordjones@gmail.com>
2015-01-26 21:30:23 +0000

    Register block icon

--- src/gifts/common/blocks/BlockPresent.java
+++ src/gifts/common/blocks/BlockPresent.java
@@ -6,9 +6,12 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockPresent extends Block
 {
@@ -29,6 +32,13 @@ public class BlockPresent extends Block
 	public int quantityDropped(Random p_149745_1_)
 	{
 		return 0;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister reg)
+	{
+		this.blockIcon = reg.registerIcon("giftsmod:blockPresent");
 	}
 
 }
